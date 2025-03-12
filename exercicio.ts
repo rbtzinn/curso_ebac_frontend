b0b23ff462a6b06c5013fb2836c42a767e03239f
Roberto Gabriel <roberto.gabriel2004@hotmail.com>
2025-03-12 14:22:46 -0300

    commit inicial

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+function saudar(nome: string): string {
+  return "Olá " + nome;
+}
+
+console.log(multiplicar(5, 3)); 
+console.log(saudar("Roberto")); 
